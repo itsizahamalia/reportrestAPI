e3e829bf474345445d5a81d94b5287e81705c60b
itsizahamalia <113334814+itsizahamalia@users.noreply.github.com>
2023-04-01 13:01:22 +0700

    Update task5.js

--- task5.js
+++ task5.js
@@ -1,7 +1,6 @@
 import http from 'k6/http';
 import { check } from "k6";
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 export const options = {
     scenarios : {
@@ -58,6 +57,5 @@ export default function() {
 export function handleSummary(data) {
     return {
       "summary.html": htmlReport(data),
-      stdout: textSummary(data, { indent: " ", enableColors: true }),
     };
   };
